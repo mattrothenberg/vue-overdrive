9e6f67430fe8133d0687bfc44926aad80658aa75
Matt Rothenberg <mattrothenberg@gmail.com>
2018-02-10 10:51:54 -0500

    Use vuex for state management

--- src/Overdrive.vue
+++ src/Overdrive.vue
@@ -1,79 +1,84 @@
 <script>
-  import ramjet from 'ramjet'
-  const components = {}
-  let readyToAnimate = false
+import ramjet from 'ramjet'
 
-  export default {
-    name: 'overdrive',
-    props: {
-      id: {
-        type: String,
-        required: true
-      },
-      tag: {
-        type: String,
-        default: 'div'
-      }
+const getPosition = (node) => {
+  const rect = node.getBoundingClientRect();
+  const computedStyle = getComputedStyle(node);
+  const marginTop = parseInt(computedStyle.marginTop, 10);
+  const marginLeft = parseInt(computedStyle.marginLeft, 10);
+  return {
+      top: (rect.top - marginTop) + ((window.pageYOffset || document.documentElement.scrollTop)) + 'px',
+      left: (rect.left - marginLeft) + 'px',
+      width: rect.width + 'px',
+      height: rect.height + 'px',
+      margin: computedStyle.margin,
+      padding: computedStyle.padding,
+      position: 'absolute'
+  }
+}
+
+export default {
+  name: 'overdrive',
+  props: {
+    id: {
+      type: String,
+      required: true 
     },
-    render (h) {
-      return h(
-        this.tag,
-        this.$slots.default
-      )
+    tag: {
+      type: String,
+      required: false,
+      default: 'div'
     },
-    watch: {
-      '$route': {
-        handler (to, from) {
-          if (to && from) {
-            readyToAnimate = true
-          }
+    duration: {
+      type: Number,
+      required: false,
+      default: 250
+    }
+  },
+  computed: {
+    comps () {
+      return this.$odStore.state.components[this.id]
+    }
+  },
+  watch: {
+    comps: {
+      handler (nv, ov) {
+        const self = this
+        if (!nv && !ov) return
+          this.$nextTick(() => {
+            const [a,b] = nv
+            const clone = a.el.cloneNode(true)
+            Object.assign(clone.style, a.prevPosition)
+            document.body.appendChild(clone)
+            ramjet.hide(b.el)
+            ramjet.hide(clone)
+            ramjet.transform(clone, b.el, {
+              duration: self.duration,
+              easing: ramjet.easeIn,
+              done () {
+                ramjet.show(b.el)
+                document.body.removeChild(clone)
+                self.$odStore.dispatch('REMOVE_COMPONENT', {id: self.id})
+              }
+            })
+          })
         },
         immediate: true
       }
-    },
-    methods: {
-      animateForward (a, b) {
-        ramjet.hide(b.$el)
-        ramjet.transform(a.$el, b.$el, {
-          duration: 250,
-          easing: ramjet.easeIn,
-          done: () => {
-            ramjet.show(b.$el)
-          }
-        })
-      },
-      animateBackward (a, b) {
-        ramjet.hide(b.$el)
-        ramjet.transform(b.$el, a.$el, {
-          duration: 250,
-          easing: ramjet.easeIn,
-          done () {
-            this.onChildRoute = false
-          }
-        })
-      }
-    },
-    mounted () {
-      components[this.id]
-        ? components[this.id].push(this)
-        : components[this.id] = [this]
-      
-      if (components[this.id].length === 2) {
-        const [a, b] = components[this.id]
-        if (!readyToAnimate) return
-        this.animateForward(a, b)
-      }
-    },
-    destroyed () {
-      const [a, b] = components[this.id]
-      this.animateBackward(a, b)
-      components[this.id].pop()
-    }
+  },
+  mounted () {
+    const el = this.$slots.default[0].elm
+    this.$odStore.dispatch('ADD_COMPONENT', {
+      id: this.id,
+      el,
+      prevPosition: getPosition(el)
+    })
+  },
+  render (h) {
+    return h(
+      this.tag,
+      this.$slots.default
+    )
   }
+}
 </script>
-
-<style scoped>
-  div {
-    display: inline-block;
-  }
-</style>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,40 @@
-import Vue from 'vue'
 import Overdrive from './Overdrive.vue'
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    components: {}
+  },
+  actions: {
+    ADD_COMPONENT ({commit}, {id, el, prevPosition}) {
+      commit('addComponent', {id, el, prevPosition})
+    },
+    REMOVE_COMPONENT ({commit}, {id}) {
+      commit('removeComponent', {id})
+    }
+  },
+  mutations: {
+    addComponent(state, {id, el, prevPosition}) {
+      if (state.components[id]) {
+        state.components[id].push({
+          id, el, prevPosition
+        })
+      } else {
+        state.components[id] = [{id, el, prevPosition}]
+      }
+    },
+    removeComponent (state, {id}) {
+      state.components[id].shift()
+    }
+  }
+})
 
 const plugin = {
-  install(Vue, options) {
-    Vue.component('Overdrive', Overdrive)
+  install (Vue) {
+    Vue.prototype.$odStore = store
+    Vue.component(Overdrive.name, Overdrive)
   }
 }
 
