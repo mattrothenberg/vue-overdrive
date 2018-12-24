4351e0ccbd8816041fe961fa51773e2523b58308
Matt Rothenberg <mrothenberg@heroku.com>
2018-12-24 15:47:40 -0500

    eslint + prettier

--- src/Overdrive.vue
+++ src/Overdrive.vue
@@ -1,32 +1,37 @@
 <script>
-import ramjet from 'ramjet'
-const components = {}
+import ramjet from "ramjet";
+const components = {};
 let returnTrip = {
-  id: '',
+  id: "",
   el: null,
   pos: null
-}
-let matchedEl = null 
-let hasBustableCache = false
+};
+let matchedEl = null;
+let hasBustableCache = false;
 const getPosition = (node, addOffset = false) => {
-  const rect = node.getBoundingClientRect()
-  const computedStyle = window.getComputedStyle(node)
-  const marginTop = parseInt(computedStyle.marginTop, 10)
-  const marginLeft = parseInt(computedStyle.marginLeft, 10)
+  const rect = node.getBoundingClientRect();
+  const computedStyle = window.getComputedStyle(node);
+  const marginTop = parseInt(computedStyle.marginTop, 10);
+  const marginLeft = parseInt(computedStyle.marginLeft, 10);
 
   return {
-    top: `${(rect.top - marginTop) + ((addOffset ? 1 : 0) * (window.pageYOffset || document.documentElement.scrollTop))}px`,
-    left: `${(rect.left - marginLeft)}px`,
+    top: `${rect.top -
+      marginTop +
+      (addOffset ? 1 : 0) *
+        (window.pageYOffset || document.documentElement.scrollTop)}px`,
+    left: `${rect.left - marginLeft}px`,
     width: `${rect.width}px`,
     height: `${rect.height}px`,
     borderRadius: computedStyle.borderRadius,
-    position: 'absolute'
-  }
-}
+    position: "absolute"
+  };
+};
 export default {
   props: {
     tag: {
-      type: String, default: 'div' },
+      type: String,
+      default: "div"
+    },
     id: {
       type: String,
       required: true
@@ -40,100 +45,94 @@ export default {
       default: ramjet.linear
     }
   },
-  data () {
+  data() {
     return {
       animating: false,
       transformer: {}
-    }
+    };
   },
   methods: {
-    cache () {
+    cache() {
       components[this.id] = {
         el: this.$slots.default,
         pos: getPosition(this.$el.firstChild)
-      }
+      };
     },
-    cloneAndAppend () {
-      const { el, pos } = components[this.id]
-      const clone = el[0].elm.cloneNode(true)
-      clone.setAttribute('data-clone', true)
+    cloneAndAppend() {
+      const { el, pos } = components[this.id];
+      const clone = el[0].elm.cloneNode(true);
+      clone.setAttribute("data-clone", true);
       Object.assign(clone.style, pos);
-      document.body.appendChild(clone)
+      document.body.appendChild(clone);
     },
-    bustCache () {
+    bustCache() {
       Object.keys(components).forEach(id => {
-        components[id] = false
-      })
+        components[id] = false;
+      });
     },
-    animate (cb = () => {}) {
-      const a = document.querySelector('[data-clone]')
-      console.log(a)
-      const b = this.$el.firstChild
-      this.animating = true
+    animate(cb = () => {}) {
+      const a = document.querySelector("[data-clone]");
+      const b = this.$el.firstChild;
+      this.animating = true;
       this.transformer = ramjet.transform(a, b, {
         duration: this.duration,
         easing: this.easing,
         appendToBody: true,
         done: () => {
-          cb(a, b)
-          this.animating = false
-          this.$emit('animation-end')
+          cb(a, b);
+          this.animating = false;
+          this.$emit("animation-end");
         }
-      })
-      ramjet.hide(a, b)
+      });
+      ramjet.hide(a, b);
     },
-    handleFirstTimeMatch () {
-      matchedEl = this.id
-      this.cloneAndAppend()
+    handleFirstTimeMatch() {
+      matchedEl = this.id;
+      this.cloneAndAppend();
       const cb = (a, b) => {
-        ramjet.show(b)
-      }
+        ramjet.show(b);
+      };
       this.$nextTick(() => {
-        this.animate(cb)
-        const clone = document.querySelector('[data-clone]')
-        document.body.removeChild(clone)
-        hasBustableCache && this.bustCache()
-        this.cache()
-      })
+        this.animate(cb);
+        const clone = document.querySelector("[data-clone]");
+        document.body.removeChild(clone);
+        hasBustableCache && this.bustCache();
+        this.cache();
+      });
     },
-    handleMatch () {
-      this.cloneAndAppend()
+    handleMatch() {
+      this.cloneAndAppend();
       const cb = (a, b) => {
-        ramjet.show(b)
-        matchedEl = null
-      }
+        ramjet.show(b);
+        matchedEl = null;
+      };
       this.$nextTick(() => {
-        this.animate(cb)
-        const clone = document.querySelector('[data-clone]')
-        document.body.removeChild(clone)
-        this.cache()
-      })
+        this.animate(cb);
+        const clone = document.querySelector("[data-clone]");
+        document.body.removeChild(clone);
+        this.cache();
+      });
     }
   },
-  mounted () {
-    const match = components[this.id]
+  mounted() {
+    const match = components[this.id];
     if (match && !matchedEl) {
-      this.handleFirstTimeMatch()
+      this.handleFirstTimeMatch();
     } else if (match && matchedEl) {
-      this.handleMatch()
-      return
+      this.handleMatch();
+      return;
     } else {
-      this.cache()
+      this.cache();
     }
   },
-  beforeDestroy () {
-    hasBustableCache = Object.keys(components).length > 1
+  beforeDestroy() {
+    hasBustableCache = Object.keys(components).length > 1;
     if (this.animating) {
-      this.transformer.teardown()
+      this.transformer.teardown();
     }
   },
-  render (h) {
-    return h(
-      this.tag,
-      [
-        this.$slots.default
-      ]
-    )
+  render(h) {
+    return h(this.tag, [this.$slots.default]);
   }
-}
+};
 </script>
--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
-import Overdrive from './Overdrive.vue'
+import Overdrive from "./Overdrive.vue";
 
-export function install (Vue) {
-	if (install.installed) return
-	install.installed = true
-	Vue.component('overdrive', Overdrive)
+export function install(Vue) {
+  if (install.installed) return;
+  install.installed = true;
+  Vue.component("overdrive", Overdrive);
 }
 
-export const VOverdrive = Overdrive
+export const VOverdrive = Overdrive;
 
 const plugin = {
-	install,
+  install,
 
-	get enabled () {
-		return state.enabled
-	},
+  get enabled() {
+    return state.enabled;
+  },
 
-	set enabled (value) {
-		state.enabled = value
-	},
-}
+  set enabled(value) {
+    state.enabled = value;
+  }
+};
 
 // Auto-install
-let GlobalVue = null
-if (typeof window !== 'undefined') {
-	GlobalVue = window.Vue
-} else if (typeof global !== 'undefined') {
-	GlobalVue = global.Vue
+let GlobalVue = null;
+if (typeof window !== "undefined") {
+  GlobalVue = window.Vue;
+} else if (typeof global !== "undefined") {
+  GlobalVue = global.Vue;
 }
 if (GlobalVue) {
-	GlobalVue.use(plugin)
+  GlobalVue.use(plugin);
 }
 
-export default plugin
+export default plugin;
